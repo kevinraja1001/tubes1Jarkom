deb1fff8936fc0c76ac5f13b8aba0d78698e79d6
kevinraja <13514060@std.stei.itb.ac.id>
2016-10-06 00:39:00 +0700

    tambah total memory

--- node.c
+++ node.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <pthread.h>
 
-#define PORT 49491
+#define PORT 4949
 #define SA struct sockaddr
 #define MAX 100
 
@@ -21,7 +21,7 @@ void *fungsi (int socketfd){
 		bzero(buff, MAX);
 		read(socketfd, buff, sizeof(buff));
 		strcpy(dariClient, buff);
-		printf("Dari client: %s", dariClient);
+		//printf("Dari client: %s", dariClient);
 		bzero(buff, MAX);
 		if(strncmp("cap", dariClient, 3) == 0){		
 			strcpy(buff, "cap multigraph dirtyconfig\n");
@@ -39,7 +39,16 @@ void *fungsi (int socketfd){
 			write(socketfd, buff, sizeof(buff));
 		}
 		else if(strncmp("config memory", dariClient, 13) == 0){
-			strcpy(buff, "graph_args --base 1024 -l 0 --upper-limit 8271892480\n");
+			strcpy(buff, "graph_args --base 1024 -l 0 --upper-limit ");
+			FILE *fp = popen("free | awk 'FNR == 2 {print $2}'", "r");
+			if (fp == NULL) {
+				printf("Failed to run command\n" );
+				exit(1);
+			}
+			char used_buff[MAX];
+			fgets(used_buff, sizeof(used_buff)-1, fp);
+			pclose(fp);
+			strcat(buff, used_buff);
 			write(socketfd, buff, sizeof(buff));
 			bzero(buff, MAX);
 			strcpy(buff, "graph_vlabel Bytes\n");
@@ -130,7 +139,7 @@ int main(){
 		exit(0);
 	}
 	else{
-		printf("socket berhasil dibuat\n");
+		//do nothing. socket berhasil dibuat
 	}
 	bzero(&serveraddr, sizeof(&serveraddr));
 	serveraddr.sin_family = AF_INET;
@@ -141,22 +150,23 @@ int main(){
 		exit(0);
 	}
 	else{
-		printf("socket bind berhasil\n");
+		//do nothing. socket binding berhasil
 	}
 	if(listen(socketfd, 5) != 0){
 		printf("gagal mendengar\n");
 		exit(0);
 	}
 	else{
-		printf("server mendengar\n");
+		//do nothing. socket listening
 	}
 	len = sizeof(client);
 	pthread_t tid;
 	int i;
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i <= 5; i++) {
 		connectfd = accept(socketfd, (SA*)&client, &len);
 		if(connectfd >= 0){
 			pthread_create(&tid, NULL, fungsi, (void *)connectfd);
+			
 		}
 	}
 	close(socketfd);
